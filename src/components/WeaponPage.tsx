6361c278c2e864bf15013b8cb4b1bd5f39fbd9f9
migodlcrz <lorenzo.cap.delacruz@email.com>
2025-01-13 22:44:39 +0800

    Created weapon page

---
+++ src/components/WeaponPage.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import wish from "../assets/gif/wish.gif";
+
+const WeaponPage = () => {
+  const [showGif, setShowGif] = useState(false);
+
+  const handleWishNow = () => {
+    setShowGif(true);
+    setTimeout(() => {
+      setShowGif(false);
+    }, 5000);
+  };
+
+  return (
+    <div className="flex flex-col w-1/2 h-full items-center p-2">
+      <div className="text-2xl">Weapon Pull</div>
+      <div className="flex flex-col h-full w-full justify-start items-center">
+        <div>You Pulled</div>
+        {showGif && <img src={wish} alt="Wish GIF" />}
+      </div>
+      <button
+        onClick={handleWishNow}
+        className="bg-orange-100 border-[2px] border-orange-400 px-4 py-2 font-genshin rounded-full shadow-lg shadow-black hover:bg-orange-300 text-black transition-all"
+      >
+        Wish Now
+      </button>
+    </div>
+  );
+};
+
+export default WeaponPage;
